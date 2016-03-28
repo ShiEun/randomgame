0e82ef6a563313c47848f8816214a7fbd92664e5
Shieun Song <tldms0940@naver.com>
2016-03-28 20:41:06 +0900

    #2 implement input user name

--- app/src/main/java/kr/hs/emirim/tldms0940/monchicken/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/tldms0940/monchicken/MainActivity.java
@@ -2,12 +2,22 @@ package kr.hs.emirim.tldms0940.monchicken;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText mName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mName = (EditText)findViewById(R.id.name);
     }
+    protected void onResume(){
+        super.onResume();
+        mName.setText("");
+    }
+    mName.setTest("")
 }

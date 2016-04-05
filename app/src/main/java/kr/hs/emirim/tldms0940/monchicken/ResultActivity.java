5961a0387ce182575298568fc49e809cb0b5394a
Shieun Song <tldms0940@naver.com>
2016-04-05 20:51:30 +0900

    #3 add intent in ResultActivity

--- app/src/main/java/kr/hs/emirim/tldms0940/monchicken/ResultActivity.java
+++ app/src/main/java/kr/hs/emirim/tldms0940/monchicken/ResultActivity.java
@@ -1,5 +1,6 @@
 package kr.hs.emirim.tldms0940.monchicken;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
@@ -23,6 +24,11 @@ public class ResultActivity extends AppCompatActivity {
 
         mResult = (TextView) findViewById(R.id.result);
         mResult.setText("결과");
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        int age = intent.getIntExtra("age",-1);
+        mResult.setText(name+"님, 안녕하세요!");
 
         ImageView mImageView = (ImageView)findViewById(R.id.image);
         int imageId = (int) (Math.random() * images.length);
